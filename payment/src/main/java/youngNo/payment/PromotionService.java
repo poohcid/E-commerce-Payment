3b89ddbfb55fa5d253ef0d59435796d837f26bd2
t <38640424+poohcid@users.noreply.github.com>
2020-11-23 23:13:33 +0700

    Update PromotionService.java

--- payment/src/main/java/youngNo/payment/PromotionService.java
+++ payment/src/main/java/youngNo/payment/PromotionService.java
@@ -4,13 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import youngNo.payment.Model.*;
 
 public class PromotionService {
-	private final String url="localhost";
+	private final String url="http://localhost:8081/";
 	private HttpHeaders headers;
 	private HttpEntity<String> entityHeader;
 	private RestTemplate restTemplate;
@@ -23,8 +29,36 @@ public class PromotionService {
 	}
 	
 	//************************
+	@SuppressWarnings("finally")
 	public double getDiscount(Receive receive) {
-		//fake
-		return receive.getPromotions().size()*100;
+		double totalPrice = receive.getTaltalPrice();
+		double totalDiscount = 0.0;
+		ObjectMapper mapper;
+		JsonNode actualObj;
+		String responseText;
+		try {
+			for (Promotion promotion: receive.getPromotions()) {
+				responseText = restTemplate.exchange(
+						url+"promotion/"+promotion.getPromotion_id(), HttpMethod.GET, entityHeader, String.class).getBody();
+				mapper = new ObjectMapper();
+				actualObj = mapper.readTree(responseText);
+				if (actualObj.get("method").asText().equals("percent")) {
+					totalDiscount += totalPrice*(actualObj.get("amount").asDouble()/100);
+				}
+				else if (actualObj.get("method").asText().equals("direct")) {
+					totalDiscount += actualObj.get("amount").asDouble();
+				}
+			}
+			return totalDiscount;
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			return totalDiscount;
+			//return receive.getPromotions().size()*100;
+		}
 	}
 }
